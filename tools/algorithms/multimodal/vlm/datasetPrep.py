e6376f0d0899cf1b3862781c7c5eabbe3d77367a
Rezowan-Ferdous <34978101+Rezowan-Ferdous@users.noreply.github.com>
2025-10-28 00:13:25 +0000

    yaml_fixing

--- tools/algorithms/multimodal/vlm/datasetPrep.py
+++ tools/algorithms/multimodal/vlm/datasetPrep.py
@@ -3,10 +3,11 @@ import cv2 as cv
 import torch 
 import albumentations as A
 
+from omegaconf import DictConfig, OmegaConf
+import hydra
 
 
 
-import conf.data.vlm as config
 # import config as cfg
 
 from torch.utils.data import Dataset, DataLoader
@@ -27,17 +28,18 @@ class VisionLanguageDataset(Dataset):
         caption = self.captions[idx]
         tokens = self.tokenizer(caption, return_tensors="pt", padding=True, truncation=True)
         return image, tokens
-
+    
+@hydra.main(config_path="../../conf", config_name="vlm_config", version_base=None)
 class CLIPDataset(torch.utils.data.Dataset):
-    def __init__(self, image_filenames, captions, tokenizer, dataset, transform=None):
-        self.image_filenames = image_filenames
+    def __init__(self, image_path,max_length,image_size, captions, tokenizer, dataset, transform=None,):
+        self.image_path = image_path
         self.captions = list(captions)
         self.encoded_captions = tokenizer(
-            list(captions), padding=True, truncation=True, max_length=config.max_length
+            list(captions), padding=True, truncation=True, max_length=max_length
         )
         self.dataset = dataset
         self.transform = transform
-
+        self.image_size = image_size
     def __len__(self):
         return len(self.captions)
 
@@ -47,7 +49,7 @@ class CLIPDataset(torch.utils.data.Dataset):
             for key, values in self.encoded_captions.items()
         }
 
-        image = cv.imread(f"{config.image_path}/{self.image_filenames[idx]}")
+        image = cv.imread(f"{self.dataset}/{self.image_path[idx]}")
         image = cv.cvtColor(image, cv.COLOR_BGR2RGB)
         image = self.transforms(image=image)['image']
         item['image'] = torch.tensor(image).permute(2, 0, 1).float()
@@ -55,18 +57,18 @@ class CLIPDataset(torch.utils.data.Dataset):
 
         return item
     
-    def get_transforms(mode="train"):
+    def get_transforms(self, mode="train"):
         if mode == "train":
             return A.Compose(
                 [
-                    A.Resize(config.size, config.size, always_apply=True),
+                    A.Resize(self.image_size, self.image_size, always_apply=True),
                     A.Normalize(max_pixel_value=255.0, always_apply=True),
                 ]
             )
         else:
             return A.Compose(
                 [
-                    A.Resize(config.size, config.size, always_apply=True),
+                    A.Resize(self.image_size, self.image_size, always_apply=True),
                     A.Normalize(max_pixel_value=255.0, always_apply=True),
                 ]
             )
